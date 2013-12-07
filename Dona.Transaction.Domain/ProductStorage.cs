c5cb9675593e300171379df0082ec3208134622c
diedona <diedona@gmail.com>
2013-12-06 23:48:07 -0200

    Início do projeto: DOMAIN INFRA Service (iniciado)

---
+++ Dona.Transaction.Domain/ProductStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dona.Transaction.Domain
+{
+    public class ProductStorage
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+        public Storage Storage { get; set; }
+        public int StorageId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Dona.Transaction.Domain/ProductStorageLog.cs
@@ -0,0 +1,21 @@
+﻿using Dona.Transaction.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dona.Transaction.Domain
+{
+    public class ProductStorageLog
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int StorageId { get; set; }
+        public Storage Storage { get; set; }
+        public int? QuantityIn { get; set; }
+        public int? QuantityOut { get; set; }
+        public EnumLogType Type { get; set; }
+    }
+}
